2c695c91162d6f122fadf820055d792f2683d9f3
Denys Temchenko <denis.temchenko@gmail.com>
2015-03-02 16:56:55 +0000

    OOT-101 bundle skeleton added

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Oro\Bundle\BtsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('oro_bundle_bts');
+
+        return $treeBuilder;
+    }
+}
---
+++ OroBtsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\BtsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroBtsBundle extends Bundle
+{
+}
